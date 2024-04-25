e921baeec682b1925962192b1b07321f47cff17e
Martin Bahniuk <martin.bahniuk@gmail.com>
2024-04-25 13:19:44 +0300

    Switch deployment to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: '/srv/svelte-test-task',
-			assets: '/srv/svelte-test-task',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
